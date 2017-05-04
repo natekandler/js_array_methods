74d989795822a14e970eecaa54118cb5bc1f770a
Nate Kandler <natekandler@gmail.com>
2017-05-04 14:58:33 -0700

    adds examples

--- array_methods.js
+++ array_methods.js
@@ -1,3 +1,107 @@
-var returnsTrue = function(){
-  return true;
+function includes(arr, obj) {
+  var results = []
+  for(var i=0; i<arr.length; i++) {
+    if (arr[i] == obj){
+      results.push(arr[i])
+    };
+  }
+  return results
+}
+
+//function include(arr, obj) {
+//  return arr.filter(function(i){
+//    return i == obj
+//  });
+//}
+
+function greaterThan(arr, obj) {
+  var results = []
+  for(var i=0; i<arr.length; i++) {
+    if (arr[i] > obj){
+      results.push(arr[i])
+    };
+  }
+  return results
+}
+
+// second argument as a function
+// function would return property 
+function sum(arr, base_case) {
+  var results = base_case;
+  for(var i=0; i<arr.length; i++) {
+    results += arr[i];
+  }
+  return results
+}
+
+// function sum(arr) {
+//   return arr.reduce(function(acc, val) {
+//     return acc + val
+//   }, 0);
+// }
+
+function hyphenate(arr, base_case) {
+  var results = base_case;
+  for(var i=0; i<arr.length; i++) {
+    if(i === 0){
+      results += arr[i]
+    } else {
+      results += ("-" + arr[i]);
+    }
+  }
+  return results
+}
+
+// function hyphenate(arr) {
+//   return arr.reduce(function(acc, val) {
+//     if(acc === ""){
+//       return acc + val
+//     } else {
+//       return acc + ("-" +  val)
+//     }
+//   }, "");
+// }
+
+
+function timesTwo(arr) {
+  var results = []
+  for(var i=0; i<arr.length; i++) {
+      results.push(arr[i] * 2)
+    };
+  return results;
+ }
+
+function possessive(arr) {
+  var results = []
+  for(var i=0; i<arr.length; i++) {
+      results.push(arr[i] + "'s");
+    };
+  return results;
+ }
+
+function sortArray(arr) {
+   var len = arr.length;
+   for (var i = len-1; i>=0; i--){
+     for(var j = 1; j<=i; j++){
+       if(arr[j-1]>arr[j]){
+           var temp = arr[j-1];
+           arr[j-1] = arr[j];
+           arr[j] = temp;
+        }
+     }
+   }
+   return arr;
+}
+
+//function sortArray(arr) {
+//  return arr.sort()
+//}
+
+function logEach(arr) { 
+  var results = [];
+  for(var i=0; i<arr.length; i++){
+    console.log(arr[i]);
+    results.push(arr[i]);
+  }
+  return results;
 }
--- spec/array_methods_spec.js
+++ spec/array_methods_spec.js
@@ -1,9 +1,56 @@
 describe("js enumerables", function() {
-  var orange;
 
-  it("returns true", function() {
-    var result = returnsTrue();
-    console.log(result)
-    expect(result).toBe(true);
+  it("filters for matching items in array", function() {
+    var testArray = [1,2,3,2,4];
+    var result = includes(testArray, 2);
+    expect(result).toEqual([2,2]);
+  });
+
+  it("filters items greater than comparator", function() {
+    var testArray = [2,4,6,8,10];
+    var result = greaterThan(testArray, 7);
+    expect(result).toEqual([8,10]);
+  });
+
+
+  it("sums an array", function() {
+    var testArray = [1,2,3];
+    var result = sum(testArray, 0);
+    expect(result).toEqual(6);
+  });
+
+  it("adds hyphens between words", function() {
+    var testArray = ["first","second","third"];
+    var result = hyphenate(testArray, "");
+    expect(result).toEqual("first-second-third");
+  });
+
+  it("multiplies by two", function() {
+    var testArray = [2,4,6];
+    var result = timesTwo(testArray);
+    expect(result).toEqual([4,8,12]);
+  });
+
+  it("makes the string possessive", function(){
+    var testArray = ["Nate","Mark","Kevin"];
+    var result = possessive(testArray);
+    expect(result).toEqual(["Nate's","Mark's","Kevin's"]);
+
+  });
+
+
+  it("sorts and array", function(){
+    var testArray = [5,2,3,4,1]; 
+    var result = sortArray(testArray);
+    expect(result).toEqual([1,2,3,4,5])
+  });
+
+  it("should log each item and return original", function(){
+    var testArray = [1,2]
+    console.log = jasmine.createSpy("log");
+    var result = logEach(testArray);
+    expect(result).toEqual(testArray)
+    expect(console.log).toHaveBeenCalledWith(1);
+    expect(console.log).toHaveBeenCalledWith(2);
   });
 });
